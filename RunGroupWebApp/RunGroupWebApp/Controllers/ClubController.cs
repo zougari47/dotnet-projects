224f7cc4a27e93c8ff18d291a9b3171933339e73
Ahmed Zougari <zougariahmed007@gmail.com>
2022-12-30 18:12:43 +0000

    first commit

---
+++ RunGroupWebApp/RunGroupWebApp/Controllers/ClubController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Data.intefaces;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly IClubRepository _clubRepository;
+
+        public ClubController(IClubRepository clubRepository)
+        {
+            _clubRepository = clubRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Club> clubs = await _clubRepository.GetAll();
+            return View(clubs);
+        }
+
+        //[HttpGet("/")]
+        public async Task<IActionResult> Detail(int id)
+        {
+            Club club = await _clubRepository.GetByIdAsync(id);
+            return View(club);
+        }
+    }
+}
---
+++ RunGroupWebApp/RunGroupWebApp/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+    }
+}
