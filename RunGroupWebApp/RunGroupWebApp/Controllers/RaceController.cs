41a9cb9dce3c6717de1efebffeec55f82b0c8675
Ahmed Zougari <zougariahmed007@gmail.com>
2023-01-04 17:18:01 +0000

    RunGroupWebApp: add delete club/race

--- RunGroupWebApp/RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/RunGroupWebApp/Controllers/RaceController.cs
@@ -122,5 +122,22 @@ namespace RunGroupWebApp.Controllers
                 return View(raceVM);
             }
         }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var raceDetails = await _raceRepository.GetByIdAsync(id);
+            if (raceDetails == null) return View("Error");
+            return View(raceDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteRace(int id)
+        {
+            var raceDetails = await _raceRepository.GetByIdAsync(id);
+            if (raceDetails == null) return View("Error");
+
+            _raceRepository.Delete(raceDetails);
+            return RedirectToAction("Index");
+        }
     }
 }
