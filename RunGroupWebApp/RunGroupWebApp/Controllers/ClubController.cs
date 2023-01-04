dcc3f695cc24a390f4b80a15b7edac72787efcec
Ahmed Zougari <zougariahmed007@gmail.com>
2023-01-04 15:15:53 +0000

    RunGroupWebApp add edit race/club

--- RunGroupWebApp/RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/RunGroupWebApp/Controllers/ClubController.cs
@@ -24,7 +24,6 @@ namespace RunGroupWebApp.Controllers
             return View(clubs);
         }
 
-        //[HttpGet("/")]
         public async Task<IActionResult> Detail(int id)
         {
             Club club = await _clubRepository.GetByIdAsync(id);
@@ -64,6 +63,66 @@ namespace RunGroupWebApp.Controllers
             }
             return View(clubVM);
 
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var club = await _clubRepository.GetByIdAsync(id);
+            if (club == null) return View("Error");
+            var clubVM = new EditClubViewModel
+            {
+                Title = club.Title,
+                Description = club.Description,
+                AddressId = (int)club.AddressId,
+                Address = club.Address,
+                URL = club.Image,
+                ClubCategory = club.ClubCategory
+            };
+            return View(clubVM);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditClubViewModel clubVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit club");
+                return View("Edit", clubVM);
+            }
+
+            var userClub = await _clubRepository.GetByIdAsyncNoTracking(id);
+
+            if (userClub != null)
+            {
+                try
+                {
+                    await _photoService.DeletePhotoAsync(userClub.Image);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Could not delete photo");
+                    return View(clubVM);
+                }
+
+                var photoResult = await _photoService.AddPhotoAsync(clubVM.Image);
+                var club = new Club
+                {
+                    Id = id,
+                    Title = clubVM.Title,
+                    Description = clubVM.Description,
+                    Image = photoResult.Url.ToString(),
+                    AddressId = clubVM.AddressId,
+                    Address = clubVM.Address
+                };
+
+                _clubRepository.Update(club);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(clubVM);
+            }
         }
     }
 }
--- RunGroupWebApp/RunGroupWebApp/Repository/RaceRepository.cs
+++ RunGroupWebApp/RunGroupWebApp/Repository/RaceRepository.cs
@@ -41,6 +41,11 @@ namespace RunGroupWebApp.Repository
             return await _context.Races.Include(i => i.Address).FirstOrDefaultAsync(i => i.Id == id);
 
         }
+        public async Task<Race> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Races.Include(i => i.Address).AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+
+        }
 
         public bool Save()
         {
